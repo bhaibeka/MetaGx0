97994090fc16db92ffc8e671bcf332ca8c98d89e
Benjamin Haibe-Kains <bhaibeka@ircm.qc.ca>
2014-01-03 12:00:00 -0500

    prototype of subtypeSurvival

--- R/subtypeSurvival.R
+++ R/subtypeSurvival.R
@@ -10,16 +10,20 @@ function (eset, geneid, plot=TRUE, weighted=TRUE, time.cens, condensed=TRUE, res
 
   ######################
   
-  concIndex <- function (stime, sevent, strat, tau, inpudata, cv.fold, itr=1000, seed=12345) {
+  concIndex <- function (stime, sevent, strat, tau, inpudata, cv.fold, alternativeitr=1000, seed=12345) {
     
-    if (missing(tau))) { tau <- max(stime, na.rm=TRUE) }
+    if (missing(tau)) { tau <- max(stime, na.rm=TRUE) }
     if (missing(strat)) { strat <- array(1, dim=length(stime), dimnames=list(names(stime))) }
     if (length(stime) != length(sevent) || length(stime) != length(strata) || length(stime) != nrow(inpudata)) { stop("Wrong dimensions dimensions for stime, sevent, strat and inpudata") }
     if (!missing(cv.fold) && (cv.fold <= 1 || cv.fold > nrow(inpudata))) { cv.fold <- nrow(inpudata) }
     if (!is.matrix(inpudata)) { inpudata <- cbind("score"=inpudata) }
     ## use Uno C index
     if (missing(cv.fold)) {
-      cindex <- survC1::Inf.Cval(mydata=cbind("stime"=stime, "sevent"-sevent, inpudata), tau=tau, itr=itr, seed=seed)
+      ccix <- complete.cases(stime, sevent, inpudata)
+      rr <- survC1::Inf.Cval(mydata=cbind("stime"=stime, "sevent"=sevent, inpudata)[ccix, , drop=FALSE], tau=tau, itr=itr, seed=seed)
+      pp <- 2 * pnorm(abs())
+      cindex <- list("estimate"=rr$Dhat, "se"=rr$se, "lower"=rr$low95, "upper"=rr$upp95, "p-value"=pp)
+    }
   }
   
   ######################
@@ -35,8 +39,7 @@ function (eset, geneid, plot=TRUE, weighted=TRUE, time.cens, condensed=TRUE, res
   if (!file.exists(file.path(resdir))) { dir.create(file.path(resdir), showWarnings=FALSE, recursive=TRUE) }
   
   ## for a single expressionSet object
-  
-  survC1::inf.Cval
+
 }
 
 
