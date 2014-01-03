e8987a143925c9e2cd384bbc1de4e6d5620bca97
Benjamin Haibe-Kains <bhaibeka@ircm.qc.ca>
2014-01-03 12:00:52 -0500

    remove subtypeSurvival

--- R/subtypeSurvival.R
+++ R/subtypeSurvival.R
@@ -8,37 +8,7 @@
 `subtypeSurvival` <- 
 function (eset, geneid, plot=TRUE, weighted=TRUE, time.cens, condensed=TRUE, resdir="cache", nthread=1) {
 
-  ######################
-  
-  concIndex <- function (stime, sevent, strat, tau, inpudata, cv.fold, alternativeitr=1000, seed=12345) {
-    
-    if (missing(tau)) { tau <- max(stime, na.rm=TRUE) }
-    if (missing(strat)) { strat <- array(1, dim=length(stime), dimnames=list(names(stime))) }
-    if (length(stime) != length(sevent) || length(stime) != length(strata) || length(stime) != nrow(inpudata)) { stop("Wrong dimensions dimensions for stime, sevent, strat and inpudata") }
-    if (!missing(cv.fold) && (cv.fold <= 1 || cv.fold > nrow(inpudata))) { cv.fold <- nrow(inpudata) }
-    if (!is.matrix(inpudata)) { inpudata <- cbind("score"=inpudata) }
-    ## use Uno C index
-    if (missing(cv.fold)) {
-      ccix <- complete.cases(stime, sevent, inpudata)
-      rr <- survC1::Inf.Cval(mydata=cbind("stime"=stime, "sevent"=sevent, inpudata)[ccix, , drop=FALSE], tau=tau, itr=itr, seed=seed)
-      pp <- 2 * pnorm(abs())
-      cindex <- list("estimate"=rr$Dhat, "se"=rr$se, "lower"=rr$low95, "upper"=rr$upp95, "p-value"=pp)
-    }
-  }
-  
-  ######################
-  
-  if (class(eset) != "ExpressionSet") {
-    stop("Handling list of expressionSet objects is not implemented yet")
-  }
-  
-  if (missing(geneid)) {
-    geneid <- Biobase::fData(eset)[ , "ENTREZID"]
-  }
-  
-  if (!file.exists(file.path(resdir))) { dir.create(file.path(resdir), showWarnings=FALSE, recursive=TRUE) }
-  
-  ## for a single expressionSet object
+
 
 }
 
